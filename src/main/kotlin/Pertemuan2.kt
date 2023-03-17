c562ba756851916479fc91da301b00b78d498a9c
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-17 14:29:24 +0700

    latihan data 1a

--- src/main/kotlin/Pertemuan2.kt
+++ src/main/kotlin/Pertemuan2.kt
@@ -1,3 +1,14 @@
 fun main(){
-    println("Ganjar")
+    //Pertemuan1
+    //println("Ganjar")
+
+    //Pertemuan2
+    var namaLengkap: String = "Budianto"
+    var alamat: String
+    alamat = "Yogyakarta"
+    var umur = 22
+
+    println(namaLengkap)
+    println(alamat)
+    println(umur)
 }
